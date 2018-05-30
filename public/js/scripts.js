41718579617dfa11ad63c8289dd42371c4a14579
Jayce Azua <jayce.azua@gmail.com>
2018-05-30 06:32:58 -0400

    need to add the correct link to redirect to itunes store

--- public/js/scripts.js
+++
@@ -1,7 +0,0 @@
-let details = document.querySelectorAll('.moreDetails');
-
-details.forEach((detail) => {
-  detail.addEventListener('click', function(e) {
-    console.log(e.target.id);
-  });
-});
